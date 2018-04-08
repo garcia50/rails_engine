92f6c2c1e82a70f3b090e7d0b61f275c417640aa
Luis Garcia <garcia@yahoo.com>
2018-04-08 15:45:34 -0400

    Added relationships and total_amount method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,10 @@
 class Invoice < ApplicationRecord
-  
+  has_many :invoice_items
+  has_many :transactions
+
+  def total_amount
+    invoice_items.reduce(0) do |sum, item|
+      sum + item.unit_price
+    end
+  end
 end
