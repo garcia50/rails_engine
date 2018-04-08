84ec7bc0d93e966cc84512fae4a1eebbb5b44ed5
Luis Garcia <garcia@yahoo.com>
2018-04-08 15:47:53 -0400

    Add a validation and add requested methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,8 +1,29 @@
 class Merchant < ApplicationRecord
-  
+  validates_presence_of :name
 
   has_many :items
   has_many :invoices
 
   has_many :transactions, through: :invoices
+
+  def total_revenue
+    successful_transactions = transactions.successful.includes(:invoice)
+    trans = successful_transactions.reduce(0) do |sum, transaction|
+      sum + transaction.invoice.total_amount
+    end
+    "The total revenue for this merchant is $#{trans}"
+  end
+
+  def item_collection 
+    items
+  end
+
+  def invoice_collection 
+    invoices
+  end
+
+  def self.search(params)
+    find_by(params)
+  end
+
 end
