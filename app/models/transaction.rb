b9ab998754deb4e2b1c3ceb1f1e9cf2d0291fdc8
Luis Garcia <garcia@yahoo.com>
2018-04-08 15:48:45 -0400

    Add validation, relationship to invoice and create method scopes

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,8 @@
 class Transaction < ApplicationRecord
+  validates_presence_of :result
 
+  belongs_to :invoice
+
+  scope :successful,   -> { where result: 'success' }
+  scope :unsuccessful, -> { where result: 'failed' }
 end
