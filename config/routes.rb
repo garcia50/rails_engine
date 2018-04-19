6d7decbad0031552c6988f15ca04658d805d57a1
Luis Garcia <garcia@yahoo.com>
2018-04-19 11:48:27 -0400

    Update and add routes. Also remove comments.

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,40 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        get :revenue, to: 'merchants#revenue'
-        get :items, to: 'merchants#items'
-        get :invoices, to:'merchants#invoices'
-        
+        get :revenue,           to: 'merchants#revenue'
+        get :items,             to: 'merchants#items'
+        get :invoices,          to: 'merchants#invoices'
+        get :favorite_customer, to: 'merchants#favorite'
+        get :customers_with_pending_invoices, to: 'merchants#pending'
+
         collection do
-          get :find, to: 'merchants#find'
+          get :most_revenue, to: 'merchants#most_revenue'
+          get :most_items,   to: 'merchants#most_items'
+          get :find,         to: 'merchants#find'
+          get :find_all,     to: 'merchants#find_all'
         end
       end
 
+      resources :invoices, only: [:index, :show] do 
+        get :transactions, to: 'invoices#transactions'
 
-      resources :invoices, only: [:index]
+        collection do 
+          get :find, to: 'invoices#find'
+          get :find, to: 'invoices#find_all'
+        end
+      end
 
       
-      resources :customers, only: [:index, :show]
-      resources :items, only: [:index, :show]
-      resources :invoice_items, only: [:index]
-      resources :transactions, only: [:index]
+      resources :customers, only: [:index, :show] do
+        get :favorite_merchant, to: 'customers#favorite'
+      end
+
+
+
+
+      # resources :items, only: [:index, :show]
+      # resources :invoice_items, only: [:index]
+      # resources :transactions, only: [:index]
 
     end
   end
--- lib/tasks/data_import.rake
+++ lib/tasks/data_import.rake
@@ -15,18 +15,12 @@ namespace :db do
     data_models.each do |model|
       filename = "#{model.to_s.parameterize}.csv"
       path = File.join(Rails.root, "db", "data", filename)
-      
       count = 0
-      
-      # merge (update) item_id into that hash
 
       CSV.foreach(path, headers: true) do |row|
-      # turn row into a hash, save to a variable
-      # item_id = Item[0].id
         model.create(row.to_h)
         count += 1
       end
-
       puts "Saved #{count} #{model} records in the database."
     end
   end
