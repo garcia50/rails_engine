20ef81e390b67190fc72949d92e8b789b42bb107
Luis Garcia <garcia@yahoo.com>
2018-04-19 11:49:15 -0400

    Add and update merchant controller and merchant model

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,27 +8,49 @@ class Api::V1::MerchantsController < ApplicationController
     render json: Merchant.find(params[:id])
   end
 
+  def most_revenue
+    render json: Merchant.most_revenue(params[:quantity])
+  end
+
+  def most_items
+    render json: Merchant.most_items(params[:quantity])
+  end
+
   def revenue
     merchant = Merchant.find(params[:merchant_id])
     render json: merchant.total_revenue
   end
 
+  def favorite
+    merchant = Merchant.find(params[:merchant_id])
+    render json: merchant.favorite
+  end
+
   def items
     merchant = Merchant.find(params[:merchant_id])
-    render json: merchant.item_collection
+    render json: merchant.items
   end
 
   def invoices
     merchant = Merchant.find(params[:merchant_id])
-    render json: merchant.invoice_collection
+    render json: merchant.invoices
+  end
+
+  def pending
+    merchant = Merchant.find(params[:merchant_id])
+    render json: merchant.pending
   end
 
   def find
-    render json: Merchant.search(merchant_params)
+    render json: Merchant.find_by(merchant_params)
+  end
+
+  def find_all
+    render json: Merchant.where(merchant_params)
   end
 
   private def merchant_params
-    params.permit(:name, :id, :created_at, :updated_at)
+    params.require(:merchant).permit(:name, :id, :created_at, :updated_at)
   end
  
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,27 +3,46 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
-
   has_many :transactions, through: :invoices
+  has_many :customers,    through: :invoices
+
+  def self.most_revenue(limit = 5)
+    joins(invoices: [:transactions, :invoice_items])
+    .group('merchants.id')
+    .merge(Transaction.successful)
+    .order('sum(invoice_items.quantity * invoice_items.unit_price) DESC')
+    .take(limit)
+  end
+
+  def self.most_items(limit = 5)
+    joins(invoices: [:transactions, :invoice_items])
+    .group(:id)
+    .merge(Transaction.successful)
+    .order('sum(invoice_items.quantity) DESC')
+    .take(limit)
+  end
 
   def total_revenue
     successful_transactions = transactions.successful.includes(:invoice)
     trans = successful_transactions.reduce(0) do |sum, transaction|
       sum + transaction.invoice.total_amount
     end
-    "The total revenue for this merchant is $#{trans}"
+    "The total revenue for this merchant is $#{trans.to_i/100.0}"
   end
 
-  def item_collection 
-    items
+  def favorite
+    customers
+      .joins(invoices: [:transactions])
+      .merge(Transaction.successful)
+      .group('customers.id')
+      .order('count(invoices.*) desc')
+      .first
   end
 
-  def invoice_collection 
-    invoices
-  end
-
-  def self.search(params)
-    find_by(params)
+  def pending
+    customers
+    .joins("join transactions on transactions.invoice_id = invoices.id")
+    .merge(Transaction.unsuccessful).distinct
   end
 
 end
